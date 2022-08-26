5a9a264d26629773679a680309e1580b735fee59
Franco Traverso <franco-t20@outlook.com>
2022-08-25 22:02:25 -0300

    Add diff auxiliary comparison object

--- src/modules/saved-search.ts
+++ src/modules/saved-search.ts
@@ -1,7 +1,13 @@
-type SavedSearch = {
+export type SavedSearch = {
   listingType: "rent" | "sell";
   countyIds: string[];
   propertyTypes: ("house" | "land" | "condo" | "townhome")[];
 };
 
-export const ModSavedSearch = {};
+const diff = (before: SavedSearch, after: SavedSearch) => {};
+
+const ModSavedSearch = {
+  diff,
+};
+
+export default ModSavedSearch;
